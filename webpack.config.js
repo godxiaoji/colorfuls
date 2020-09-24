4c405ada9cd11fc699bf671b3125967257543abb
林永吉 <260437@gree.com.cn>
2020-09-24 16:01:24 +0800

    chore(webpack.config.js): remove banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
-const { name, version, author } = require('./package.json')
 
 module.exports = {
   entry: './src/index.js',
@@ -13,13 +11,5 @@ module.exports = {
     umdNamedDefine: true,
     globalObject: 'this'
   },
-  mode: 'production',
-  plugins: [
-    new webpack.BannerPlugin({
-      entryOnly: true, // 是否仅在入口包中输出 banner 信息
-      banner: () => {
-        return `${name}.js v${version}\nCopyright 2020 ${author}`
-      }
-    })
-  ]
+  mode: 'production'
 }
